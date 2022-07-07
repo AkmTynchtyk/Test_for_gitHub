1e449f96b2cf7f3185c74bd533b6c66b914b35b8
usertime2022 <tynchtyk7@mail.ru>
2022-07-08 00:33:01 +0600

    men AkmTynchtyk menen ozgorttum!

--- src/peaksoft/Main.java
+++ src/peaksoft/Main.java
@@ -2,5 +2,6 @@ package peaksoft;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Men usertime2022 menen ozgorttum!");
+        System.out.println("Emi men AkmTynchtyk menen ozgorttum!");
     }
 }
