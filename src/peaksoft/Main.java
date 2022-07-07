5732524b49ececd4a0402f61126007253447e6f6
usertime2022 <tynchtyk7@mail.ru>
2022-07-08 01:50:33 +0600

    3-souttu koshtum

--- src/peaksoft/Main.java
+++ src/peaksoft/Main.java
@@ -3,5 +3,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Men usertime2022 menen ozgorttum!");
         System.out.println("Emi men AkmTynchtyk menen ozgorttum!");
+
+        System.out.println("Men AkmTynchtyk menen feature/add_sout3 dep jany branch achtym da ushul souttu koshtum");
     }
 }
